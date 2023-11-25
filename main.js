3cf1956a8d41a7209c89f11c24c3a81a68eab9ec
Dave Conco <118613296+dconco@users.noreply.github.com>
2023-11-25 06:31:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ Form.addEventListener('submit', async (e) => {
 
 
     const url = 'https://davebot.infinityfreeapp.com/server.php'
+    const proxyUrl = 'https://davebot.infinityfreeapp.com/proxy.php?url=' + encodeURIComponent(url);
     const controller = new AbortController()
 
     // cancel request if it's more than 20 seconds
@@ -44,7 +45,7 @@ Form.addEventListener('submit', async (e) => {
 
     // Send a POST request to server
     const response = await axios({
-        url: url,
+        url: proxyUrl,
         method: 'POST',
         signal: controller.signal,
 
